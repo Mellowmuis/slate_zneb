f5c3b8421155246050afdbf179e72e3f28a4ab96
Marten Sytema <marten@sytematic.nl>
2015-04-16 16:42:12 +0200

    ..

--- dev/php/templates/template-app.php
+++ dev/php/templates/template-app.php
@@ -52,7 +52,7 @@ Template Name: App page
 					<br /><br/>
 					Voor Apple iPhone gebruikers: scan de QR-code of surf naar de website. <br /><br />
 
-					<a href="#">Download de app hier</a>
+					<a href="https://play.google.com/store/apps/details?id=com.wZneb">Download de app hier</a>
 
 
 
